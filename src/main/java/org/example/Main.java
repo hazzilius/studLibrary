f22bd0be7a9955088ff47ad98035285ac9f60b2a
hazzilius <hazzi7778@gmail.com>
2024-12-04 01:49:51 +0300

    Функция добавления книг

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,17 @@
 package org.example;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
 
 public class Main extends Operations {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        List<Book> listOfBooks;
+        List<Book> listOfBooks = new ArrayList<>();
         File saveFile = new File("books.json");
         if (saveFile.exists()) {
-            // Что то
-        } else {
-            listOfBooks = new ArrayList<>();
+            listOfBooks = getBooksFromJson();
         }
 
         while (true) {
@@ -30,6 +29,36 @@ public class Main extends Operations {
             } else {
                 System.out.println("---------------------------------------");
                 switch (option) {
+                    case 1:
+                        //Добавить книгу
+                        System.out.println("ДОБАВЛЕНИЕ КНИГИ\n");
+                        System.out.print("Название: ");
+                        scanner.nextLine();
+                        String title = scanner.nextLine();
+                        System.out.print("Автор: ");
+                        String author = scanner.nextLine();
+                        System.out.print("Издательство: ");
+                        String publisher = scanner.nextLine();
+                        System.out.print("Год издания книги: ");
+                        int year = scanner.nextInt();
+                        System.out.print("Раздел библиотеки: ");
+                        scanner.nextLine();
+                        String section = scanner.nextLine();
+                        Book book = new Book(author, title, publisher, year, section);
+
+                        listOfBooks.add(book);
+                        try {
+                            saveBooksInJson(listOfBooks);
+                        } catch (JsonProcessingException e) {
+                            System.out.println(e.getMessage());
+                        }
+                        break;
+                    case 2:
+                        //Удалить книгу
+                        break;
+                    case 3:
+                        //Поиск по книгам
+                        break;
                     default:
                         System.out.println("Неправильный ввод!");
                         break;
--- src/main/java/org/example/Operations.java
+++ src/main/java/org/example/Operations.java
@@ -1,6 +1,24 @@
 package org.example;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.*;
 
 public class Operations{
+    public static List<Book> getBooksFromJson(){
+        List<Book> books = new ArrayList<>();
+        //TODO
+        return books;
+    }
 
+    public static void saveBooksInJson(List<Book> books) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(books);
+        try (FileWriter fw = new FileWriter("books.txt", false)){
+            fw.write(json);
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
