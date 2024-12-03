4df2904c2ef1c6b171f6d750f10dcbd3f0f48414
305-11 <hazzi7778@gmail.com>
2024-12-03 13:49:46 +0300

    Добавлено меню

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,41 @@
 package org.example;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 
-public class Main {
+public class Main extends Operations {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Book> listOfBooks;
+        File saveFile = new File("books.json");
+        if (saveFile.exists()) {
+            // Что то
+        } else {
+            listOfBooks = new ArrayList<>();
+        }
+
+        while (true) {
+            //Меню
+            System.out.print("""
+                1) Добавить книгу
+                2) Удалить книгу
+                3) Поиск по книгам
+                4) Выход из программы
+               """);
+            System.out.print("Выберите действие: ");
+            int option = scanner.nextInt();
+            if (option == 4) {
+                break;
+            } else {
+                System.out.println("---------------------------------------");
+                switch (option) {
+                    default:
+                        System.out.println("Неправильный ввод!");
+                        break;
+                }
+                System.out.println("---------------------------------------");
+            }
+        }
     }
 }
---
+++ src/main/java/org/example/Operations.java
@@ -0,0 +1,6 @@
+package org.example;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Operations{
+
+}
